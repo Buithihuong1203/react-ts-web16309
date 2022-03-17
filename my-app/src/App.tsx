ac4491b2e813c444e911d4918f4c013dcff5040c
huongbtph16610 <buithihuong20080@gmail.com>
2022-03-17 15:36:50 +0700

    buổi 3

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,23 +1,72 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import ShowInfo from './components/ShowInfo';
+import ShowInfo from './components/ShowInfo'
+
 import type { Product } from './types/product';
+import { list } from './api/product';
+import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
+import AdminLayout from './pages/layouts/AdminLayout';
+import WebsiteLayout from './pages/layouts/WebsiteLayout';
+import Dashboard from './pages/Dashboard';
+import ProductManager from './pages/ProductManager';
+
+
+//function App() {
+//  const [count, setCount] = useState<number>(0)
+//  const [info, setInfo] = useState<Product>({
+////    name: "Huong",
+//    age: 21
+//  });
+
+//  return (
+//    <div className='App'>
+//      <ShowInfo person={info} />
+//      {count}<button onClick={() => (setCount((count) => count + 1))}>Click</button>
+
+//    </div>
+////  )
+//}
+
+//export default App
+
 
 function App() {
-  const [count, setCount] = useState<number>(0)
-  const [info, setInfo] = useState<Product>({
-    name: "Huong",
-    age: 21
-  });
+    const [products, setProducts] = useState<{ _id: number, name: string }[]>([])
 
-  return (
-    <div className='App'>
-      <ShowInfo person={info} />
-      {count}<button onClick={() => (setCount((count) => count + 1))}>Click</button>
-
-    </div>
-  )
+    useEffect(() => {
+        const getProducts = async () => {
+            const { data } = await list();
+            setProducts(data);
+        }
+        getProducts();
+    })
+    return (
+        <div className="App">
+            <header>
+                <ul>
+                    <li><NavLink to="/">Home Page</NavLink></li>
+                    <li><NavLink to="/product">Product</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                </ul>
+            </header>
+            <main>
+                <Routes>
+                    <Route path="/" element={<WebsiteLayout />}>
+                        <Route index element={<HomePage />} />
+                        <Route path="product" element={<ProductPage />} />
+                    </Route>
+                    <Route path="admin" element={<AdminLayout />}>
+                        <Route index element={<Navigate to="/admin/dashboard" />} />
+                        <Route path="dashboard" element={<Dashboard />} />
+                        <Route path="product" element={<ProductManager />} />
+                    </Route>
+                </Routes>
+            </main>
+        </div>
+    )
 }
 
 export default App
---
+++ my-app/src/pages/layouts/WebsiteLayout.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const WebsiteLayout = () => {
+    return (
+        <div>
+            <header>
+                Header Website
+            </header>
+            <main>
+                <Outlet />
+
+            </main>
+            <footer>
+                Footer website
+            </footer>
+        </div>
+    )
+}
+
+export default WebsiteLayout
