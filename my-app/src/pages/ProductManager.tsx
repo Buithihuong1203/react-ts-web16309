ac70a2c20f361248698686918ddfd01a8240e46e
huongbtph16610 <buithihuong20080@gmail.com>
2022-03-23 11:10:11 +0700

    feat: add, remove, update

--- my-app/src/pages/ProductManager.tsx
+++ my-app/src/pages/ProductManager.tsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import { ProductType } from '../types/product'
+import { Link } from 'react-router-dom'
+
+
 type ProductMangagerProps = {
     products: ProductType[];
+    onRemove: (id: number) => void
 }
 
 const ProductManager = (props: ProductMangagerProps) => {
     return (
         <div>
-            <table>
+            <button onClick={() => props.onAdd()}>Thêm mới</button>
+            <table className="table table-bordered">
                 <tbody>
                     {props.products.map((item, index) => {
                         return <tr>
                             <td>{index + 1}</td>
                             <td>{item.name}</td>
                             <td>{item.price}</td>
-                            <td><button>Remove</button></td>
+                            <td>
+                                <Link to={`/admin/product/${item.id}/edit`}>Edit</Link>
+                                <button onClick={() => props.onRemove(item.id)}>Remove</button>
+                            </td>
                         </tr>
                     })}
                 </tbody>
